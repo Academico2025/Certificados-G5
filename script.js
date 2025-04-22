3e95c375042e3ced884e91780efd7f6aa7d73294
Academico2025 <adrianalopezxxi@gmail.com>
2025-04-22 18:56:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // Simulación de base de datos de certificados
 const certificates = {
     2023: [
-        { id: "123456789", name: "***********", file: "cert_2023_123456789.pdf" },
-        { id: "987654321", name: "***********", file: "cert_2023_987654321.pdf" }
+        { id: "123456789", name: "***********", file: "certificados/2023/cert_2023_123456789.pdf" },
+        { id: "987654321", name: "***********", file: "certificados/2023/cert_2023_987654321.pdf" }
     ],
     2024: [
-        { id: "123456789", name: "Juan Pérez", file: "cert_2024_123456789.pdf" },
-        { id: "987654321", name: "María Gómez", file: "cert_2024_987654321.pdf" }
+        { id: "123456789", name: "Juan Pérez", file: "certificados/2024/cert_2024_123456789.pdf" },
+        { id: "987654321", name: "María Gómez", file: "certificados/2024/cert_2024_987654321.pdf" }
     ],
     2025: [
-        { id: "1059362653", name: "BURBANO MUÑOZ DEILY YASBETH", file: "BURBANO_MUÑOZ_DEILY_YASBETH.pdf" },
-        { id: "1059363247", name: "DELACRUZ BENAVIDES VALERY SOFIA", file: "DELACRUZ BENAVIDES VALERY SOFIA.pdf" },
-	{ id: "1110054600", name: "GALINDEZ FREIRE SANTIAGO", file: "GALINDEZ_FREIRE_SANTIAGO.pdf" },
-	{ id: "1059362995", name: "IMBACHI ORDOÑEZ BRIDNY ROXANA", file: "IMBACHI ORDOÑEZ BRIDNY ROXANA.pdf" },
-	{ id: "1059363057", name: "RODRIGUEZ BASTIDAS ANTHONY YELSID", file: "RODRIGUEZ BASTIDAS ANTHONY YELSID.pdf" },
+        { id: "1059362653", name: "BURBANO MUÑOZ DEILY YASBETH", file:"certificados/2025/BURBANO_MUÑOZ_DEILY_YASBETH.pdf" },
+        { id: "1059363247", name: "DELACRUZ BENAVIDES VALERY SOFIA", file:"certificados/2025/DELACRUZ BENAVIDES VALERY SOFIA.pdf" },
+	{ id: "1110054600", name: "GALINDEZ FREIRE SANTIAGO", file: "certificados/2025/GALINDEZ_FREIRE_SANTIAGO.pdf" },
+	{ id: "1059362995", name: "IMBACHI ORDOÑEZ BRIDNY ROXANA", file: "certificados/2025/IMBACHI ORDOÑEZ BRIDNY ROXANA.pdf" },
+	{ id: "1059363057", name: "RODRIGUEZ BASTIDAS ANTHONY YELSID", file: "certificados/2025/RODRIGUEZ BASTIDAS ANTHONY YELSID.pdf" },
     ],
     2026: [
-        { id: "123456789", name: "**********", file: "cert_2026_123456789.pdf" },
-        { id: "987654321", name: "**********", file: "cert_2026_987654321.pdf" }
+        { id: "123456789", name: "**********", file: "certificados/2026/cert_2026_123456789.pdf" },
+        { id: "987654321", name: "**********", file: "certificados/2026/cert_2026_987654321.pdf" }
     ],
     2027: [
-        { id: "123456789", name: "**********", file: "cert_2027_123456789.pdf" },
-        { id: "987654321", name: "**********", file: "cert_2027_987654321.pdf" }
+        { id: "123456789", name: "**********", file: "certificados/2027/cert_2027_123456789.pdf" },
+        { id: "987654321", name: "**********", file: "certificados/2027/cert_2027_987654321.pdf" }
     ],
     2028: [
-        { id: "123456789", name: "**********", file: "cert_2028_123456789.pdf" },
-        { id: "987654321", name: "**********", file: "cert_2028_987654321.pdf" }
+        { id: "123456789", name: "**********", file: "certificados/2028/cert_2028_123456789.pdf" },
+        { id: "987654321", name: "**********", file: "certificados/2028/cert_2028_987654321.pdf" }
     ]
 };
 
